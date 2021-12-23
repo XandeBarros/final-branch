869cc319fd025868dd56159b8e99ac81ff26aea3
Matheus Della Rocca Martins <84210251+MatheusDrm@users.noreply.github.com>
2021-12-23 14:35:33 -0300

    Implementação da interface com o usuário

--- Compressor.cpp
+++ Compressor.cpp
@@ -41,6 +41,7 @@ private:
   string lineComp;
   array<int, 4> numBinary = {0, 0, 0, 0};
   int key = -1;
+  int aux = 1, aux2; //auxiliar na interface
 
   // Métodos
 
@@ -65,14 +66,14 @@ private:
     int handleNumber;
 
     handleNumber = numberDecimal;
-  
+
     int i = 4;
     while (numberDecimal > 0) {
       i--;
       arr[i] = numberDecimal % 2;
       numberDecimal = numberDecimal / 2;
     }
-    
+
     while (i != 0) {
       i--;
       arr[i] = 0;
@@ -91,6 +92,7 @@ public:
     if(arqInicial.is_open()) {
       while(getline(arqInicial, line)) {
         if(line.empty()) break;
+        aux2 = 1;
         for(int i = 0; i < line.length(); i++) {
           int init = 4 * i;
           if(init >= line.length()) break;
@@ -103,6 +105,8 @@ public:
             int numberByToDec = binaryToDecimal(strToNumberBy);
             char letter = TranslateTable[numberByToDec + 13];
 
+            interfaceCompactar(subString, numberByToDec, letter);
+            aux2 = 0;
             arqCompactado << letter;
           } else {
             arqCompactado << subString;
@@ -124,16 +128,15 @@ public:
     int len = arq.size() - 4;
     string arqComp = arq.substr(0, len) + "Compactado.txt";
     string arqDesc = arq.substr(0, len) + "Descompactado.txt";
-
     arqCompactadoLeitura.open(arqComp);
     arqDescompactado.open(arqDesc);
-
+    aux = 1; //Auxiliar da interface com usuário
     if(arqCompactadoLeitura.is_open()) {
       int lines = 0;
       while(getline(arqCompactadoLeitura, lineComp)) {
         lines++;
         for(int i = 0; i < lineComp.length(); i++) {
-          string subStringDesc = lineComp.substr(i, 1);
+          string subStringDesc = lineComp.substr(i, 1); //Lê um elemento por vez do arquivo compactado
           stringstream ss;
           for (auto &i : TranslateTable) {
             if (i.second == subStringDesc[0]) {
@@ -153,14 +156,17 @@ public:
             arqDescompactado << charToNumber;
             continue;
           }
-        
+
           array<int, 4> result = decimalToBinary(charToNumber, numBinary);
-          
+
           for(int i : result)
             ss << i;
-          
+
           string resultado;
           ss >> resultado;
+
+          interfaceDescompactar(resultado, subStringDesc);
+
           arqDescompactado << resultado;
         }
         arqDescompactado << "\n";
@@ -174,6 +180,30 @@ public:
 
     return true;
   };
+
+  void interfaceCompactar(string binario, int decimal, char simbolo){
+    if (aux==1){
+        cout << endl << "*** Processo de compactacao ***" << endl << endl;
+        aux = 0;
+    }
+    if (aux2==1){
+        cout << "Vetor lido: " << line << endl;
+    }
+    cout << "   Sequencia identificada: " << binario << endl;
+    cout << "   Numero correspondente em decimal: " << decimal << endl;
+    cout << "   Simbolo correspondente: " << simbolo << endl;
+    cout << endl;
+  }
+
+  void interfaceDescompactar(string sequencia, string simbolo){
+    if (aux==1){
+        cout << endl << "*** Processo de descompactacao ***" << endl << endl;
+        aux = 0;
+    }
+    cout << "Simbolo lido: " << simbolo << endl;
+    cout << "   Sequencia correspondente: " << sequencia << endl;
+    cout << endl;
+  }
 };
 
 int main() {
