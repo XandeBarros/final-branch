08802df9c8bf226ae4b98fda0f665b25fe8349f0
Alexandre Barros de Araújo <barrosalex6@gmail.com>
2022-01-08 21:10:14 -0300

    arquivo main concentra a Interface e o Compressor

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include"Compressor.hpp"
+#include"Interface.hpp"
+using namespace std;
+
+int main() {
+  Compressor cmp;
+  Interface interface;
+  string arquivoCmp, arquivoDmp;
+  bool loop = true;
+  
+  while(loop) {
+    int menuOp = interface.menu();
+    if (menuOp == 3) break;
+
+    switch (menuOp) {
+      case 1:
+        cout << "  Digite o nome do arquivo que vai ser compactado: ";
+        cin >> arquivoCmp;
+
+        if(cmp.compactar(arquivoCmp)) {
+          cout << "  -> Arquivo compactado com sucesso!" << endl;
+        } else {
+          cout << "  -> Erro ao compactar" << endl;
+        }
+
+        break;
+      case 2:
+        cout << "  Digite o nome do arquivo que vai ser descompactado: ";
+        cin >> arquivoDmp;
+
+        if(cmp.descompactar(arquivoDmp)) {
+          cout << "  -> Arquivo descompactado com sucesso!" << endl;
+        } else {
+          cout << "  -> Erro ao descompactar" << endl;
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
+  return 0;
+}
